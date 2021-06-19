7bd97123e8cc707ec136d9bb29fe8f7d6b960c78
Subaiyal Sheikh <subaiyalshk@gmail.com>
2021-06-19 16:13:26 -0500

    hero text fixed

--- react/src/components/about.jsx
+++ react/src/components/about.jsx
@@ -1,4 +1,12 @@
+import {useRef} from 'react'
+
 export const About = (props) => {
+  const contractAdd = useRef(null)
+
+  const onclickHandler = () => {
+    // contractAdd.value;
+  }
+
   return (
     <div id='about' class=" text-center">
       <div className='container'>
@@ -8,7 +16,9 @@ export const About = (props) => {
               <h2 class="jumbotron-heading">Why Saint ?</h2>
               <p class="lead">There are more than 2.2 billion children in the world. Many of them are stricken by poverty, sickness, and/or abuse. Most of the problems faced by children can be mitigated through research, product development, and volunteerism, and there are many great organizations working to make an impact, however available funds, or the lack thereof, is an issue. Saint Token is on a mission to change this!</p>
               <p class="lead">We are consistently donating funds every Friday</p>
-              <p class="lead contract-address">0x503576189Edd9FEb058ad3f17f1E6e9172A11D75</p>
+              <textarea id="contract-address" ref={contractAdd} value="0x503576189Edd9FEb058ad3f17f1E6e9172A11D75" style={{'color':'black'}}></textarea>
+              
+              <button onClick={onclickHandler}>Copy address</button>
               <a
                 href='https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x503576189Edd9FEb058ad3f17f1E6e9172A11D75'
                 className='btn btn-custom1 btn-lg '
